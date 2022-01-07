5878743e2ed997b0fa4bb68304de18b5f4cb6c35
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2022-01-07 04:52:45 +0200

    N Bytes

--- src/sample/HuffmanDecoder.java
+++ src/sample/HuffmanDecoder.java
@@ -6,10 +6,12 @@ public class HuffmanDecoder {
 
     private static int numOfNodes;
     private static String encodedTree;
+    private static String n="";
+    private static int NUM_BYTES;
 
     public static void main(String[] args) throws IOException {
 
-        File fileinput = new File("E:\\Java Projects\\HuffmanCompression\\output1.bin");
+        File fileinput = new File("E:\\Java Projects\\HuffmanCompression\\output.bin");
         FileInputStream fileInputStream = new FileInputStream(fileinput);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
 
@@ -33,10 +35,12 @@ public class HuffmanDecoder {
 
         encodedTree = readFile.toString();
 
-//        numOfNodes = Integer.parseInt(result.substring(0,8),2);
-//        System.out.println(numOfNodes);
-//        result = result.substring(8);
-//        encodedTree = result;
+        NUM_BYTES = Integer.parseInt(encodedTree.substring(0,8),2);
+        encodedTree = encodedTree.substring(8);
+        //Final character in encoded string
+        for(int i=0;i<NUM_BYTES;i++){
+            n+="ÿ";
+        }
 
 //        Pad
 //        int length = encodedTree.length();
@@ -45,6 +49,8 @@ public class HuffmanDecoder {
 //            length++;
 //        }
         Node headNode = readNode();
+
+
 
         String decoded = decode(headNode,encodedTree);
         System.out.println(decoded);
@@ -61,8 +67,11 @@ public class HuffmanDecoder {
         encodedTree = encodedTree.substring(1);
         if (c == '1') {
                 numOfNodes--;
-                String nodeData = String.valueOf((char) Integer.parseInt(encodedTree.substring(0, 8), 2));
-                encodedTree = encodedTree.substring(8);
+                String nodeData="";
+                for(int i=0;i<NUM_BYTES;i++) {
+                    nodeData += String.valueOf((char) Integer.parseInt(encodedTree.substring(0, 8), 2));
+                    encodedTree = encodedTree.substring(8);
+                }
                 return new Node(nodeData, null, null, 0);
         }
         else{
@@ -96,7 +105,7 @@ public class HuffmanDecoder {
             if(c=='0'){
                 decodingNode = decodingNode.getLeft();
                 if(decodingNode.getLeft()==null && decodingNode.getRight()==null){
-                    if(!decodingNode.getCharacter().equals("ÿ")) {
+                    if(!decodingNode.getCharacter().equals(n)) {
                         decoded.append(decodingNode.getCharacter());
                         decodingNode = node;
                     }
@@ -105,7 +114,7 @@ public class HuffmanDecoder {
             if(c=='1'){
                 decodingNode = decodingNode.getRight();
                 if(decodingNode.getLeft()==null && decodingNode.getRight()==null){
-                    if(!decodingNode.getCharacter().equals("ÿ")) {
+                    if(!decodingNode.getCharacter().equals(n)) {
                         decoded.append(decodingNode.getCharacter());
                         decodingNode = node;
                     }
